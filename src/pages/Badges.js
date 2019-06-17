ffe0c6d05421e6f1ec716a32622dc5b3621ce1df
kengreg <vgregory@fishing-v.co.jp>
2019-06-17 20:21:24 +0900

    Added functions in each cycle of life of the component. On componentUnmont I added a new clearsetime to make sure even if we go to other page

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -5,38 +5,78 @@ import BadgesList from "../components/BadgesList";
 import {Link} from 'react-router-dom';
 
 class Badges extends React.Component{
-    state = {
-        data:[
-            {
-                "id": "id01",
-                "firstName": "Warrior of Light",
-                "lastName": "None",
-                "serie": "Final Fantasy I",
-                "jobTitle": "warrior",
-                "twitter": "@twitteando",
-                "avatarUrl": "https://cache.sqex-bridge.jp/img/oPG2NHftm"
-            },
-            {
-                "id": "id02",
-                "firstName": "Cloud",
-                "lastName": "Strife",
-                "serie": "Final Fantasy VII",
-                "jobTitle": "Soldier",
-                "twitter": "@twitteando",
-                "avatarUrl": "https://cache.sqex-bridge.jp/img/oPGsN7kqxJ"
-            },
-            {
-                "id": "id03",
-                "firstName": "Onion",
-                "lastName": "Knight",
-                "serie": "Final Fantasy III",
-                "jobTitle": "light warrior??",
-                "twitter": "@twitteando",
-                "avatarUrl": "https://cache.sqex-bridge.jp/img/oPGdNre4M"
-            }
-        ]
+    //the constructor need props to init the super(), so write down props wwwwww
+    constructor(props){
+        super(props);
+        //the states need to be here in the constructor, you cant put just "state" you need to put "this.state", if not then it will not pass the info to the render.
+        this.state = {
+            data:[]
+        }
+        console.log('1. constructor()')
     }
+    componentDidMount(){
+        console.log('3.didmount ()')
+        //Here we put a set time of 3 seg to start the function where will update the "this.state" with a new information from an API or any file.
+        setTimeout(() => {
+            //to avoid the error of a function working while we went to other page so the component doesnt exist, we need to use the "timeoutId" to make a clearTimeout using the id
+            //Fixing 1> first we save the set time or function which need to start in a constant inside of this. so this.timeoutId
+                this.timeoutId = setTimeout(() => {
+
+                        this.setState({
+                            data:[
+                                {
+                                    "id": "id01",
+                                    "firstName": "Warrior of Light",
+                                    "lastName": "None",
+                                    "serie": "Final Fantasy I",
+                                    "jobTitle": "warrior",
+                                    "twitter": "@twitteando",
+                                    "avatarUrl": "https://cache.sqex-bridge.jp/img/oPG2NHftm"
+                                },
+                                {
+                                    "id": "id02",
+                                    "firstName": "Cloud",
+                                    "lastName": "Strife",
+                                    "serie": "Final Fantasy VII",
+                                    "jobTitle": "Soldier",
+                                    "twitter": "@twitteando",
+                                    "avatarUrl": "https://cache.sqex-bridge.jp/img/oPGsN7kqxJ"
+                                },
+                                {
+                                    "id": "id03",
+                                    "firstName": "Onion",
+                                    "lastName": "Knight",
+                                    "serie": "Final Fantasy III",
+                                    "jobTitle": "light warrior??",
+                                    "twitter": "@twitteando",
+                                    "avatarUrl": "https://cache.sqex-bridge.jp/img/oPGdNre4M"
+                                }
+                            ]
+                        })
+                }, 3000)
+
+    });
+        
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        console.log("5.componenDidUpdate()")
+        console.log({
+            prevProps: prevProps, prevState: prevState
+        })
+        console.log({
+            props:this.props,
+            state: this.state
+        })
+    }
+    componentWillUnmount (){
+        console.log("6.willUnmount")
+        //Fixing > 2 Then we use the constant id in the clear time
+        clearTimeout(this.timeoutId)
+    }
+
     render(){
+        console.log('2.render ()')
         return(
             <React.Fragment>
 
